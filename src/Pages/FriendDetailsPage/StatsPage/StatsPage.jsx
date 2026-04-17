ef71077cc503d850e617f3da966a99c6f8997291
kazisamin0173-byte <kazisamin0173@gmail.com>
2026-04-17 21:07:06 +0600

    stat no data function add

--- src/Pages/FriendDetailsPage/StatsPage/StatsPage.jsx
+++ src/Pages/FriendDetailsPage/StatsPage/StatsPage.jsx
@@ -1,8 +1,10 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { Legend, Pie, PieChart, Tooltip, ResponsiveContainer } from "recharts";
 import { FriendContext } from "../../../ContextApi/CreateContext";
 
 const StatsPage = () => {
+  const navigate = useNavigate();
   const { calladd } = useContext(FriendContext);
 
   const callCount = calladd.filter(item => item.type === "call").length ;
@@ -16,7 +18,36 @@ const StatsPage = () => {
   ];
 
   return (
-    <div className="max-w-[1110px] bg-[#FFFFFF] rounded-lg shadow-lg p-8 mt-20 mx-auto">
+    
+    <div className="max-w-[1110px] mt-20 mx-auto">
+
+  {calladd.length === 0 ? (
+
+    
+    <div className="bg-gray-50 border border-dashed border-gray-300 rounded-xl p-16 text-center max-w-3xl mx-auto">
+      
+      <h2 className="text-3xl font-semibold mb-3">
+        No Data to Visualize
+      </h2>
+
+      <p className="text-gray-500 mb-6">
+        Your interaction statistics will appear here once you start making calls or sending messages.
+      </p>
+
+      <button
+        onClick={() => navigate("/timeline")}
+        className="bg-[#244D3F] text-white px-6 py-2 rounded-lg"
+      >
+        View Timeline
+      </button>
+
+    </div>
+
+  ) : (
+
+    
+    <div className="bg-white rounded-lg shadow-lg p-8">
+      
       <h2 className="text-5xl mb-6 text-[#1F2937] font-bold">
         Friendship Analytics
       </h2>
@@ -33,14 +64,18 @@ const StatsPage = () => {
               cornerRadius="50%"
               paddingAngle={5}
               dataKey="value"
-              isAnimationActive={true}
             />
             <Legend />
             <Tooltip />
           </PieChart>
         </ResponsiveContainer>
       </div>
+
     </div>
+
+  )}
+
+</div>
   );
 };
 
